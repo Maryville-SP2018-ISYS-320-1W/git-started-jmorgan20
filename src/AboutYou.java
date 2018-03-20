cf7eea8ae82ffc63347ec4eab115650e6ac6bad8
jmorgan20 <37450743+jmorgan20@users.noreply.github.com>
2018-03-19 20:50:02 -0500

    assignment

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("Jason");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("To learn about Java");
 	}
 
 }
